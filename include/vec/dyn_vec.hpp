a548889fe948c617914b6e87935102c1df187241
k.konishi <53860933+Konishi2911@users.noreply.github.com>
2023-12-12 14:51:35 +0900

    [ADD] add a class representing dynamic-sized linear algebric vector

---
+++ include/vec/dyn_vec.hpp
@@ -0,0 +1,128 @@
+#pragma once
+#include <vector>
+
+namespace lalib {
+
+/// Represents a compile-time fixed size vector.
+template<typename T>
+struct DynVec {
+public:
+    // === Initializations === //
+
+    /// @brief Create a sized vector with given array with copy.
+    constexpr DynVec(const std::vector<T>& vec) noexcept: 
+        _elems(vec) {}
+
+    /// @brief Create a sized vector with given array with move.
+    constexpr DynVec(std::vector<T>&& vec) noexcept: 
+        _elems( std::move(vec) ) {}
+
+    /// @brief A copy constructor
+    constexpr DynVec(const DynVec<T>& vec) noexcept = default;
+
+    /// @brief A move constructor
+    constexpr DynVec(DynVec<T>&& vec) noexcept = default;
+
+    /// @brief Creates a sized vector filled with the given value.
+    static constexpr auto filled(size_t count, T value) noexcept -> DynVec<T>;
+
+
+    // === Assignment === //
+
+    /// @brief Replaces the elements of the vector.
+    /// @param vec the vector to use as data source
+    /// @return a reference of the vector after modified by the operation.
+    constexpr auto operator=(const DynVec<T>& vec) noexcept -> DynVec<T>& = default;
+
+    /// @brief Replaces the elements of the vector 
+    /// @param vec the vector to use as data source
+    /// @return a reference of the vector after modified by the operation.
+    constexpr auto operator=(DynVec<T>&& vec) noexcept -> DynVec<T>& = default;
+
+
+    // === Inspecting === //
+
+    /// @brief Returns the number of elements.
+    constexpr auto size() const noexcept -> size_t;
+
+
+    // === Indexing and Iterators === //
+    using Iter = std::vector<T>::iterator;
+    using ConstIter = std::vector<T>::const_iterator;
+
+    constexpr auto begin() noexcept -> Iter;
+    constexpr auto begin() const noexcept -> ConstIter;
+    constexpr auto cbegin() const noexcept -> ConstIter;
+    constexpr auto end() noexcept-> Iter;
+    constexpr auto end() const noexcept -> ConstIter;
+    constexpr auto cend() const noexcept -> ConstIter;
+
+    constexpr auto operator[](size_t i) const -> const T&;
+    constexpr auto operator[](size_t i) -> T&;
+
+private:
+    std::vector<T> _elems;
+};
+
+
+template <typename T>
+inline constexpr auto DynVec<T>::filled(size_t count, T value) noexcept -> DynVec<T> {
+    return DynVec( std::vector(count, value) );
+}
+
+template <typename T>
+inline constexpr auto DynVec<T>::size() const noexcept -> size_t {
+    return this->_elems.size();
+}
+
+template <typename T>
+inline constexpr auto DynVec<T>::begin() noexcept -> Iter
+{
+    return this->_elems.begin();
+}
+
+template <typename T>
+inline constexpr auto DynVec<T>::begin() const noexcept -> ConstIter
+{
+    return this->cbegin();
+}
+
+template <typename T>
+inline constexpr auto DynVec<T>::cbegin() const noexcept -> ConstIter
+{
+    return this->_elems.cbegin();
+}
+
+template <typename T>
+inline constexpr auto DynVec<T>::end() noexcept -> Iter
+{
+    return this->_elems.end();
+}
+
+template <typename T>
+inline constexpr auto DynVec<T>::end() const noexcept -> ConstIter
+{
+    return this->cend();
+}
+
+template <typename T>
+inline constexpr auto DynVec<T>::cend() const noexcept -> ConstIter
+{
+    return this->_elems.cend();
+}
+
+template <typename T>
+inline constexpr auto DynVec<T>::operator[](size_t i) const -> const T &
+{
+    return this->_elems[i];
+}
+
+template <typename T>
+inline constexpr auto DynVec<T>::operator[](size_t i) -> T& {
+    return this->_elems[i];
+}
+
+
+// === Specializations of utility templates === //
+
+}
---
+++ test/vec/dyn_vec.cc
@@ -0,0 +1,67 @@
+#include "../../include/vec/dyn_vec.hpp"
+#include <iterator>
+#include <ranges>
+#include <gtest/gtest.h>
+
+//static_assert(std::random_access_iterator<typename lalib::SizedVec<uint32_t, 3>::Iter>);
+//static_assert(std::random_access_iterator<typename lalib::SizedVec<uint32_t, 3>::ConstIter>);
+
+TEST(DynVecTests, CopyConstTest) {
+    auto vec = lalib::DynVec<uint32_t>({0, 1, 2});
+    auto vec_copy = vec;
+
+    ASSERT_EQ(vec_copy[0], vec[0]);
+    ASSERT_EQ(vec_copy[1], vec[1]);
+    ASSERT_EQ(vec_copy[2], vec[2]);
+}
+
+TEST(DynVecTests, MoveConstTest) {
+    auto vec = lalib::DynVec<uint32_t>({0, 1, 2});
+    auto vec_addr = std::vector<const uint32_t*>();
+    std::ranges::transform(vec, std::back_inserter(vec_addr), [](const uint32_t& e) { return &e; });
+    auto vec_move = std::move(vec);
+
+    ASSERT_EQ(vec_addr[0], &vec_move[0]);
+    ASSERT_EQ(vec_addr[1], &vec_move[1]);
+    ASSERT_EQ(vec_addr[2], &vec_move[2]);
+}
+
+TEST(DynVecTests, CopyAssignTest) {
+    auto vec = lalib::DynVec<uint32_t>({0, 1, 2});
+    auto vec_copy = lalib::DynVec<uint32_t>({2, 1, 0});
+    vec_copy = vec;
+
+    ASSERT_EQ(vec_copy[0], vec[0]);
+    ASSERT_EQ(vec_copy[1], vec[1]);
+    ASSERT_EQ(vec_copy[2], vec[2]);
+}
+
+TEST(DynVecTests, MoveAssignTest) {
+    auto vec = lalib::DynVec<uint32_t>({0, 1, 2});
+    auto vec_move = lalib::DynVec<uint32_t>({2, 1, 0});
+    auto vec_addr = std::vector<const uint32_t*>();
+    std::ranges::transform(vec, std::back_inserter(vec_addr), [](const uint32_t& e) { return &e; });
+    vec_move = std::move(vec);
+
+    ASSERT_EQ(vec_addr[0], &vec_move[0]);
+    ASSERT_EQ(vec_addr[1], &vec_move[1]);
+    ASSERT_EQ(vec_addr[2], &vec_move[2]);
+}
+
+TEST(DynVecTests, SizeTest) {
+    auto vec = lalib::DynVec<uint32_t>::filled(3, 0);
+    ASSERT_EQ(3, vec.size());
+}
+
+TEST(DynVecTests, FilledTest) {
+    auto vec = lalib::DynVec<uint32_t>::filled(3, 1);
+    ASSERT_EQ(1, vec[0]);
+    ASSERT_EQ(1, vec[1]);
+    ASSERT_EQ(1, vec[2]);
+
+    auto vec4 = lalib::DynVec<uint32_t>::filled(4, 1);
+    ASSERT_EQ(1, vec4[0]);
+    ASSERT_EQ(1, vec4[1]);
+    ASSERT_EQ(1, vec4[2]);
+    ASSERT_EQ(1, vec4[3]);
+}
