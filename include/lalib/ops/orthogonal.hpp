d0fdf126bdb59f53e2e8ed5a8d05099d95332c20
k.konishi <53860933+Konishi2911@users.noreply.github.com>
2024-03-27 17:38:24 +0900

    [MOD] add debug assertion

--- include/lalib/ops/orthogonal.hpp
+++ include/lalib/ops/orthogonal.hpp
@@ -10,12 +10,14 @@
 namespace lalib::orth {
 
 /// @brief  Preforms the conventional Gram Schmidt (CGS) orthogonalization.
-template<std::ranges::input_range C>
+template<std::ranges::random_access_range C>
 requires Vector<std::ranges::range_value_t<C>>
 inline void cgs(C& vecs) {
+    auto n = vecs[0].size();
     auto m = vecs.size();
 
     for (auto j = 1u; j < m; ++j) {
+        assert(n == vecs[1].size());
         auto vj = vecs[j];
         for (auto k = 0u; k < j; ++k) {
             lalib::axpy(-lalib::dot(vecs[k], vj) / vecs[k].dot(vecs[k]), vecs[k], vecs[j]);
