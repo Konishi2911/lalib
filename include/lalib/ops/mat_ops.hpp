153ea6a987318caab180c13430e2eebb47435778
k.konishi <53860933+Konishi2911@users.noreply.github.com>
2024-06-12 19:39:03 +0900

    [ADD] add some operations for matrix

---
+++ include/lalib/ops/mat_ops.hpp
@@ -0,0 +1,60 @@
+#pragma once
+#include <type_traits>
+#ifndef LALIB_MAT_OPS_HPP
+#define LALIB_MAT_OPS_HPP
+
+#include "lalib/mat/sized_mat.hpp"
+#include "lalib/mat/dyn_mat.hpp"
+#include "vec_ops_core.hpp"
+
+namespace lalib {
+
+template<typename T, size_t N, size_t M>
+inline auto scale(const T& alpha, SizedMat<T, N, M>& mat) noexcept -> SizedMat<T, N, M> {
+    auto [n, m] = mat.shape();
+    scal_core(alpha, mat.data(), n * m);
+    return mat;
+}
+
+template<typename T>
+inline auto scale(const T& alpha, DynMat<T>& mat) noexcept -> DynMat<T> {
+    auto [n, m] = mat.shape();
+    scal_core(alpha, mat.data(), n * m);
+    return mat;
+}
+
+
+template<typename T, size_t N, size_t M>
+inline auto operator*(const T& alpha, const SizedMat<T, N, M>& mat) noexcept -> SizedMat<T, N, M> {
+    auto rmat = mat;
+    scale(alpha, rmat);
+    return rmat;
+}
+
+template<typename T>
+inline auto operator*(const T& alpha, const DynMat<T>& mat) noexcept -> DynMat<T> {
+    auto rmat = mat;
+    scale(alpha, rmat);
+    return rmat;
+}
+
+template<typename T, size_t N, size_t M>
+requires std::is_integral_v<T> || std::is_floating_point_v<T>
+inline auto operator-(const SizedMat<T, N, M>& mat) noexcept -> SizedMat<T, N, M> {
+    auto rmat = mat;
+    scale(-1.0, rmat);
+    return rmat;
+}
+
+template<typename T>
+requires std::is_integral_v<T> || std::is_floating_point_v<T>
+inline auto operator-(const DynMat<T>& mat) noexcept -> DynMat<T> {
+    auto rmat = mat;
+    scale(-1.0, rmat);
+    return rmat;
+}
+
+
+}
+
+#endif
