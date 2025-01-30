64f6ed8985dcb558421a46d0d2d18e32be4f2fc6
k.konishi <53860933+Konishi2911@users.noreply.github.com>
2025-01-30 17:42:10 +0900

    [ADD] implement SpMat class with constructors and tests for CSR matrix

--- include/lalib/mat/sp_mat.hpp
+++ include/lalib/mat/sp_mat.hpp
@@ -100,6 +100,105 @@ private:
 };
 
 
+/// @brief Sparse matrix in compressed sparse row format
+template<typename T>
+struct SpMat {
+    using ElemType = T;
+
+    // ==== Initializations ==== //
+
+    /// @brief Create an empty sparse matrix object.
+    SpMat() noexcept = default;
+
+    /// @brief Create a sparse matrix from a COO matrix.
+    SpMat(const SpCooMat<T>& mat) noexcept;
+
+    /// @brief Create a sparse matrix from a COO matrix.
+    SpMat(SpCooMat<T>&& mat) noexcept;
+
+    /// @brief Create a sparse matrix with given data.
+    SpMat(const std::vector<T>& val, const std::vector<size_t>& row_ptr, const std::vector<size_t>& col_ids);
+
+    /// @brief Create a sparse matrix with given data.
+    SpMat(std::vector<T>&& val, std::vector<size_t>&& row_ptr, std::vector<size_t>&& col_ids);
+
+    /// @brief Copy constructor
+    SpMat(const SpMat<T>& mat) noexcept = default;
+
+    /// @brief Move constructor
+    SpMat(SpMat<T>&& mat) noexcept = default;
+
+
+    // === Inspecting === //
+
+    /// @brief Returns the shape of the matrix (row, column).
+    /// @return     a pair of size_t representing the shape of the matrix.
+    constexpr auto shape() const noexcept -> std::pair<size_t, size_t>;
+
+    /// @brief Returns the number of non-zero elements in the matrix.
+    /// @return     the number of non-zero elements.
+    constexpr auto nnz() const noexcept -> size_t;
+
+    /// @brief Returns the value of the element at the given position.
+    /// @param i    row index
+    /// @param j    column index
+    /// @return     the value of the element at the given position.
+    auto operator()(size_t i, size_t j) const noexcept -> const T&;
+
+
+    // === Modifying === //
+
+    /// @brief Reserves memory for the matrix.
+    /// @param n    the number of rows to reserve
+    /// @param nnz  the number of non-zero elements to reserve
+    void reserve(size_t n, size_t nnz) noexcept {
+        this->_val.reserve(nnz);
+        this->_row_ptr.reserve(n + 1);
+        this->_col_ids.reserve(nnz);
+    }
+
+
+    // === Assignment === //
+
+    /// @brief Replaces the elements of the vector.
+    /// @param mat the matrix to use as data source
+    /// @return a reference of the matrix after modified by the operation.
+    constexpr auto operator=(const SpMat<T>& mat) noexcept -> SpMat<T>&;
+
+    /// @brief Replaces the elements of the vector.
+    /// @param mat the matrix to use as data source
+    /// @return a reference of the matrix after modified by the operation.
+    constexpr auto operator=(SpMat<T>&& mat) noexcept -> SpMat<T>&;
+
+
+    // === Accessing === //
+
+    /// @brief Returns a pointer to the array of the values.
+    /// @return     a pointer to the array of the values.
+    auto values() const noexcept -> const std::vector<T>
+        { return this->_val; }
+    
+    /// @brief Returns a pointer to the array of the row pointers.
+    /// @return     a pointer to the array of the row pointers.
+    auto row_pointers() const noexcept -> const std::vector<size_t>
+        { return this->_row_ptr; }
+
+    /// @brief Returns a pointer to the array of the column indices.
+    /// @return     a pointer to the array of the column indices.
+    auto col_indices() const noexcept -> const std::vector<size_t>
+        { return this->_col_ids; }
+
+
+private:
+    std::vector<T> _val;
+    std::vector<size_t> _row_ptr;
+    std::vector<size_t> _col_ids;
+
+    const T _zero = Zero<T>::value();
+};
+
+
+// === COO Matrix === //
 // === Implementations === //
 
 template<typename T>
@@ -157,6 +256,102 @@ template<typename T>
 constexpr auto SpCooMat<T>::operator=(SpCooMat<T>&& mat) noexcept -> SpCooMat<T>& {
     this->_val = std::move(mat._val);
     this->_row_ids = std::move(mat._row_ids);
+    this->_col_ids = std::move(mat._col_ids);
+
+    return *this;
+}
+
+
+// === CSR Matrix === //
+// === Implementations === //
+
+inline void _convert_coo_crs(std::vector<size_t>& row_ptr, const std::vector<size_t>& row_ids) {
+    size_t nnz = row_ids.size();
+
+    row_ptr.reserve(nnz);
+    if (!row_ptr.empty()) {
+        row_ptr.clear();
+    }
+
+    row_ptr.emplace_back(0);
+
+    auto it = row_ids.begin();
+    for (size_t i = 0; it < row_ids.end(); ++i) {
+        it = std::upper_bound(it, row_ids.end(), i, std::less<size_t>{});
+        row_ptr.emplace_back(std::distance(row_ids.begin(), it));
+    }
+}
+
+template<typename T>
+SpMat<T>::SpMat(const SpCooMat<T>& mat) noexcept
+    : _val(mat.values()), _row_ptr(), _col_ids(mat.col_indices())
+{
+    _convert_coo_crs(this->_row_ptr, mat.row_indices());
+}
+
+template<typename T>
+SpMat<T>::SpMat(SpCooMat<T>&& mat) noexcept
+    : _val(std::move(mat.values())), _row_ptr(), _col_ids(std::move(mat.col_indices()))
+{
+    _convert_coo_crs(this->_row_ptr, mat.row_indices());
+}
+
+template<typename T>
+SpMat<T>::SpMat(const std::vector<T>& val, const std::vector<size_t>& row_ptr, const std::vector<size_t>& col_ids)
+    : _val(val), _row_ptr(row_ptr), _col_ids(col_ids) 
+{
+    if (this->_val.size() != this->_col_ids.size()) {
+        throw std::runtime_error("The size of the vectors must be the same.");
+    }
+}
+
+template<typename T>
+SpMat<T>::SpMat(std::vector<T>&& val, std::vector<size_t>&& row_ptr, std::vector<size_t>&& col_ids)
+    : _val(std::move(val)), _row_ptr(std::move(row_ptr)), _col_ids(std::move(col_ids))
+{
+    if (this->_val.size() != this->_col_ids.size()) {
+        throw std::runtime_error("The size of the vectors must be the same.");
+    }
+}
+
+
+
+template<typename T>
+constexpr auto SpMat<T>::shape() const noexcept -> std::pair<size_t, size_t> {
+    size_t nrow = this->_row_ptr.size() - 1;
+    size_t ncol = *std::ranges::max_element(this->_col_ids) + 1;
+
+    return std::make_pair(nrow, ncol);
+}
+
+template<typename T>
+constexpr auto SpMat<T>::nnz() const noexcept -> size_t {
+    return this->_val.size();
+}
+
+template<typename T>
+auto SpMat<T>::operator()(size_t i, size_t j) const noexcept -> const T& {
+    for (auto cnt: std::views::iota(this->_row_ptr[i], this->_row_ptr[i + 1])) {
+        if (this->_col_ids[cnt] == j) {
+            return this->_val[cnt];
+        }
+    }
+    return this->_zero;
+}
+
+template<typename T>
+constexpr auto SpMat<T>::operator=(const SpMat<T>& mat) noexcept -> SpMat<T>& {
+    this->_val = mat._val;
+    this->_row_ptr = mat._row_ptr;
+    this->_col_ids = mat._col_ids;
+
+    return *this;
+}
+
+template<typename T>
+constexpr auto SpMat<T>::operator=(SpMat<T>&& mat) noexcept -> SpMat<T>& {
+    this->_val = std::move(mat._val);
+    this->_row_ptr = std::move(mat._row_ptr);
     this->_col_ids = std::move(mat._col_ids);
 
     return *this;
--- test/mat/sp_mat.cc
+++ test/mat/sp_mat.cc
@@ -100,3 +100,126 @@ TEST(SpMatTests, SpCooMatNnzTest) {
 
     ASSERT_EQ(nnz, 4);
 }
+
+
+// === CSR Matrix === //
+
+TEST(SpMatTests, SpMatCreationTest) {
+    using SpMatD = lalib::SpMat<double>;
+
+    EXPECT_NO_THROW({
+        SpMatD(
+            { 1.0, 2.0, 3.0, 4.0 },
+            { 0, 2, 4 },
+            { 0, 1, 0, 1 }
+        );
+    });
+
+    /* ------------- */
+    /* 1.0  2.0  0.0 */
+    /* 3.0  0.0  0.0 */
+    /* 0.0  4.0  5.0 */
+    /* ------------- */
+    EXPECT_NO_THROW({
+        SpMatD(
+            { 1.0, 2.0, 3.0, 4.0, 5.0 },
+            { 0, 2, 3, 5 },
+            { 0, 1, 0, 1, 2 }
+        );
+    });
+
+    EXPECT_THROW({
+        SpMatD(
+            { 1.0, 2.0, 3.0, 4.0 },
+            { 0, 2, 4 },
+            { 0, 1, 0 }
+        );
+    }, std::runtime_error);
+}
+
+TEST(SpMatTests, SpMatCopyTest) {
+    auto mat = lalib::SpMat<double> {
+        { 1.0, 2.0, 3.0, 4.0 },
+        { 0, 2, 4 },
+        { 0, 1, 0, 1 }
+    };
+
+    auto mat_copy = mat;
+
+    ASSERT_EQ(mat_copy(0, 0), mat(0, 0));
+    ASSERT_EQ(mat_copy(0, 1), mat(0, 1));
+    ASSERT_EQ(mat_copy(1, 0), mat(1, 0));
+    ASSERT_EQ(mat_copy(1, 1), mat(1, 1));
+}
+
+TEST(SpMatTests, SpMatCopyAssignTest) {
+    auto mat = lalib::SpMat<double> {
+        { 1.0, 2.0, 3.0, 4.0 },
+        { 0, 2, 4 },
+        { 0, 1, 0, 1 }
+    };
+    auto mat_copy = lalib::SpMat<double> {};
+
+    mat_copy = mat;
+
+    ASSERT_EQ(mat_copy(0, 0), mat(0, 0));
+    ASSERT_EQ(mat_copy(0, 1), mat(0, 1));
+    ASSERT_EQ(mat_copy(1, 0), mat(1, 0));
+    ASSERT_EQ(mat_copy(1, 1), mat(1, 1));
+}
+
+TEST(SpMatTests, SpMatShapeTest) {
+    auto mat = lalib::SpMat<double>(
+        { 1.0, 2.0, 3.0, 4.0, 5.0 },
+        { 0, 2, 3, 5 },
+        { 0, 1, 0, 1, 2 }
+    );
+
+    auto shape = mat.shape();
+
+    ASSERT_EQ(shape.first, 3);
+    ASSERT_EQ(shape.second, 3);
+}
+
+TEST(SpMatTests, SpMatNnzTest) {
+    auto mat = lalib::SpMat<double> {
+        { 1.0, 2.0, 3.0, 4.0 },
+        { 0, 2, 4 },
+        { 0, 1, 0, 1 }
+    };
+
+    auto nnz = mat.nnz();
+
+    ASSERT_EQ(nnz, 4);
+}
+
+TEST(SpMatTests, CooCrsConversionTest) {
+    auto coo_mat = lalib::SpCooMat<double> {
+        { 1.0, 2.0, 3.0, 4.0, 5.0 },
+        { 0, 0, 1, 2, 2 },
+        { 0, 1, 0, 1, 2 }
+    };
+
+    auto crs_mat = lalib::SpMat<double>(coo_mat);
+    auto crs_mat_moved = lalib::SpMat<double>(std::move(coo_mat));
+
+    ASSERT_EQ(crs_mat(0, 0), 1.0);
+    ASSERT_EQ(crs_mat(0, 1), 2.0);
+    ASSERT_EQ(crs_mat(0, 2), 0.0);
+    ASSERT_EQ(crs_mat(1, 0), 3.0);
+    ASSERT_EQ(crs_mat(1, 1), 0.0);
+    ASSERT_EQ(crs_mat(1, 2), 0.0);
+    ASSERT_EQ(crs_mat(2, 0), 0.0);
+    ASSERT_EQ(crs_mat(2, 1), 4.0);
+    ASSERT_EQ(crs_mat(2, 2), 5.0);
+
+    ASSERT_EQ(crs_mat_moved(0, 0), 1.0);
+    ASSERT_EQ(crs_mat_moved(0, 1), 2.0);
+    ASSERT_EQ(crs_mat_moved(0, 2), 0.0);
+    ASSERT_EQ(crs_mat_moved(1, 0), 3.0);
+    ASSERT_EQ(crs_mat_moved(1, 1), 0.0);
+    ASSERT_EQ(crs_mat_moved(1, 2), 0.0);
+    ASSERT_EQ(crs_mat_moved(2, 0), 0.0);
+    ASSERT_EQ(crs_mat_moved(2, 1), 4.0);
+    ASSERT_EQ(crs_mat_moved(2, 2), 5.0);
+}
